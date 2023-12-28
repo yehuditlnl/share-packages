50f7cb3916c1d4359e50cfc183c4c815adf90218
Yehudit Lanel <yehudit@phone.do>
2023-12-28 17:04:13 +0200

    Move the index.ts file

--- index.ts
+++
@@ -1,5 +0,0 @@
-// Export the SortingCriterion class from sorting-criterion.ts
-export { default as SortingCriterion } from './src/sorting-criterion';
-
-// Export the SortingManager class from sorting-manager.ts
-export { SortingManager } from './src/sorting-manager';
