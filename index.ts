8a855b0a2c126fbfc3825992698e798138573f84
Yehudit Lanel <yehudit@phone.do>
2023-12-28 16:56:16 +0200

    Add file

---
+++ index.ts
@@ -0,0 +1,5 @@
+// Export the SortingCriterion class from sorting-criterion.ts
+export { default as SortingCriterion } from './src/sorting-criterion';
+
+// Export the SortingManager class from sorting-manager.ts
+export { SortingManager } from './src/sorting-manager';
