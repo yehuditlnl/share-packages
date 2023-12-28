26710aa50864168fa7021b8ccb7d542f5c05cbdf
Yehudit Lanel <yehudit@phone.do>
2023-12-28 17:16:21 +0200

    try basic pakcage

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-// Export the SortingCriterion class from sorting-criterion.ts
-export { default as SortingCriterion } from "./sorting-criterion";
-
-// Export the SortingManager class from sorting-manager.ts
-export { SortingManager } from "./sorting-manager";
